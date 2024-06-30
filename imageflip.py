35e903f4ac92f3657485899d0b367d9bb33f3ad7
rich <richram79@gmail.com>
2024-06-30 12:21:25 +0530

    my second commit

--- imageflip.py
+++ imageflip.py
@@ -2,8 +2,8 @@ import gradio as gr
 import numpy as np
 
 def img_flip(img):
-    return np.flip(img, axis=0)
-    #return np.flip(img, axis=1)
+    # return np.flip(img, axis=0)
+    return np.flip(img, axis=1)
 
 
 imgflip = gr.Interface(img_flip, inputs=['image'], outputs=['image'])
